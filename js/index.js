0ef871fa3fb1a575fa61f86ac00ba4940abcedca
danajung38 <dana.jung38@gmail.com>
2026-01-08 18:00:50 +0900

    파일 변경

--- js/index.js
+++ js/index.js
@@ -8,7 +8,38 @@ $(function () {
 
     setInterval(leftmove, 6000);
 
+    var benefitSrc = '';
+
+    $('.reflet_benefits a').click(function (evt) {
+
+        evt.preventDefault();
+
+        //var benefitSrc = $(this).attr('href');
+        benefitSrc = $(this).attr('href');
+        console.log(benefitSrc);
+
+        //모달창('.popup) 등장, 이미지 박스('.benefit') 등장
+        $('.popup').show();
+        //$('.benefit').hide();
+        $(benefitSrc).show();
+
+    });
+
+    //모달창 닫기
+    $('.popup').click(function () {
+        //$(this).hide().children().find(benefitSrc).hide();
+        //$(this).hide().find(benefitSrc).hide();
+        $(this).hide().children().hide();
+    });
+
+    setInterval(slidemove, 3000);
+
+    //slidemove();
+
+    // 2~4초 마다 깜빡임 반복
+    setInterval(blink, 2000 + Math.random() * 2000);
 });
+
 
 /* ////////////////////
 함수명: leftmove
@@ -21,7 +52,7 @@ function leftmove() {
 
     //슬라이드 한칸 크기 구하기
     var liw = $('.main-image').width();
-    console.log('슬라이드 한칸 크기' + liw); //1006
+    //console.log('슬라이드 한칸 크기' + liw); //1006
 
     $('.gallery').animate({
         //marginLeft: '-=' + liw
@@ -36,3 +67,36 @@ function leftmove() {
     });
 
 }
+
+/* ////////////////////
+함수명: slidemove
+기능: card 슬라이드를 왼쪽으로 이동
+///////////////////////*/
+
+function slidemove() {
+
+    //슬라이드 한칸 크기 구하기 
+    var cardwi = $('.card').outerWidth(true);
+    console.log('box한칸 크기: ' + cardwi); //373
+
+    $('.cards_box').animate({
+        //marginLeft: -(cardwi + 9)
+        marginLeft: -cardwi
+    }, 1000, function () {
+
+        $('.card').first().appendTo('.cards_box');
+        $('.cards_box').css('margin-left', 0);
+    });
+
+}
+
+function blink() {
+    $('.eyeani .eyes').hide();
+    $('.eyeani .lid').show();
+    setTimeout(function () {
+        $('.eyeani .lid').hide();
+        $('.eyeani .eyes').show();
+    }, 120); // 실제 깜빡임 지속시간(ms)
+}
+
+
